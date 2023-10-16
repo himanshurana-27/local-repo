d461371cc630733cbf83f94935ca248203ff692e
himanshurana-27 <himanshurana0627@gmail.com>
2023-10-16 21:53:29 +0530

    Code updated for Oct 16 class

--- main.py
+++ main.py
@@ -4,7 +4,7 @@
 todos = []
 # here, the while loop
 while 1:
-    user_prompt = input('Type add or show or exit or edit: ')
+    user_prompt = input('Type add or show or exit or edit or complete: ')
     user_prompt = user_prompt.strip()  # this is used to avoid the space error like 'add ' f
     # this match is like switch case of the c and c++
     # it is the decision-making
@@ -13,8 +13,8 @@ while 1:
             todo = input('Enter the todo: ')
             todos.append(todo.title())
         case 'show' | 'display':
-            for item in todos:
-                print(item.capitalize())    
+            for index,item in enumerate(todos):
+                print(f"{index+1}. {item.capitalize()}")
         case 'edit':
             number = int(input("Number of the todo to edit: "))  # here, int is used to convert the input str to int
             number = number - 1
@@ -22,6 +22,9 @@ while 1:
             todos[int(number)] = new_todo
         case 'exit':
             break
+        case 'complete':
+            number = int(input("Number of the todo to complete: "))
+            todos.pop(number-1)
         case whatever:  # this whatever displays when you enter the unknown value in the user_prompt
             print('Hey, you enter the unknown command')
     # task -= 1
